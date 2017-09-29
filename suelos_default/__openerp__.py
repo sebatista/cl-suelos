9c837e9a8fa8a1ee3127e659fe63c56ff6a93269
jobiols <jorge.obiols@gmail.com>
2017-09-29 13:56:24 -0300

    [REM] eliminar dependencias

--- suelos_default/__openerp__.py
+++ suelos_default/__openerp__.py
@@ -59,8 +59,6 @@ Customización Suelos
         #        'ticket_citi_fix',          # corrige citi para pv impresor fiscal
         'product_unique_default_code',  # impide que se duplique el default_code
         'hide_messaging',  # oculta el menu de mensajeria
-#        'base_multi_store',  # agrega capacidad de multitienda analogo a multicompañia
-        'account_multi_store', 'base_multi_store', 'stock_multi_store',  # capacidad de limitar los diarios segun los stores (repo journal-constraint)
         'voucher_payment_check_fix',  # evita que aparezca cheques propios en medios de pago de cliente
         'account_invoice_tax_auto_update',  # autocalcula los impuestos al salvar asi no hay que hacer el update
         'server_mode', # disable some functions when running databases on odoo servers with parameter server_mode = "some value"
